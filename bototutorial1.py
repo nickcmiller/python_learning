d2ffc9592e7333336663b948d9c060501a3a14a4
Nick Miller <nickmiller@nicks-mbp-2.lan>
2022-10-11 20:46:48 -0700

    updating bucket name

--- bototutorial1.py
+++ bototutorial1.py
@@ -1,6 +1,6 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("totaltechnology")
+bucket=aws_resource.Bucket("totaltechnolog34543534")
 response = bucket.create(
     ACL='public-read'
 )
