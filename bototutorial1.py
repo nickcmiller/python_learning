483b9b26345ba92e7be31a1ea5f843ece7770b25
Nick Miller <nickmiller@nicks-mbp-2.lan>
2022-10-11 20:48:35 -0700

    make a private bucket

--- bototutorial1.py
+++ bototutorial1.py
@@ -1,8 +1,8 @@
 import boto3
 aws_resource=boto3.resource("s3")
-bucket=aws_resource.Bucket("totaltechnolog34543534")
+bucket=aws_resource.Bucket("totaltechnolog345435341")
 response = bucket.create(
-    ACL='public-read'
+    ACL='private'
 )
 
 print(response)
