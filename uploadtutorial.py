bd00c11b43cfd8229b9d180e41da638c501887d6
Nick <nick@example.com>
2022-10-13 13:47:12 -0700

    update uploadtutorial

--- uploadtutorial.py
+++ uploadtutorial.py
@@ -1,6 +1,6 @@
 import boto3
 s3_resource=boto3.client("s3")
-s3_resource.uploadfile(
+s3_resource.upload_file(
     Filename="testfile.txt",
     Bucket="totaltechnolog345435341",
     Key="testfile.txt"
