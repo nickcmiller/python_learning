205bc44fbc9eea0ada3ca48061deac4c6b81b4c7
Nick <nick@example.com>
2022-10-13 13:34:16 -0700

    update creationreadtutorial

--- creationreadtutorial.py
+++ creationreadtutorial.py
@@ -1,7 +1,7 @@
 import boto3
 s3_resource=boto3.client("s3")
 list_buckets=s3_resource.list_buckets()
+creation_date = list_buckets["Buckets"][0]["CreationDate"]
 
-
-result = list_buckets["Buckets"][0]["CreationDate"]
+result = creation_date.strftime("%d/%m/%y")
 print(result)
