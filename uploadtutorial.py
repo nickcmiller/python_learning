e70cf01e1ec6c83d50043d2cd9d9a1bc8d7e8501
Nick <nick@example.com>
2022-10-13 14:04:31 -0700

    update uploadtutorial

--- uploadtutorial.py
+++ uploadtutorial.py
@@ -1,7 +1,21 @@
 import boto3
+import os
+import glob
+
 s3_resource=boto3.client("s3")
 s3_resource.upload_file(
     Filename="testfile.txt",
     Bucket="totaltechnolog345435341",
     Key="testfile.txt"
 )
+
+n=0
+cwd=os.getcwd()
+while n < 4:
+    print(n, cwd, glob.glob("*.txt"))
+    s3_resource.upload_file(
+        Filename="testfile.txt",
+        Bucket="totaltechnolog345435341",
+        Key="testfile"+n+".txt"
+    )
+    n+=1
