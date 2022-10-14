4bcd166eae44d98573dbb4957065c41d1aefd7cc
Nick <nick@example.com>
2022-10-13 18:40:49 -0700

    update deleteS3object

--- deleteS3object.py
+++ deleteS3object.py
@@ -1,4 +1,12 @@
 import boto3
-s3_resource=boto3.client("s3")
+import os
+import glob
 
-s3_resource.delete_object(Bucket="totaltechnolog345435341", Key="testfile3.txt")
+s3_resource=boto3.client("s3")
+#s3_resource.delete_object(Bucket="totaltechnolog345435341", Key="testfile3.txt")
+
+objects = s3_resource.list_objects(Bucket="totaltechnolog345435341")["Contents"]
+
+for object in objects:
+    print(object["Key"])
+
