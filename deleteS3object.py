42e1de971cb2208ca8cd575acedeb5b34cbe39d3
Nick <nick@example.com>
2022-10-13 18:41:53 -0700

    update deleteS3object

--- deleteS3object.py
+++ deleteS3object.py
@@ -9,4 +9,6 @@ objects = s3_resource.list_objects(Bucket="totaltechnolog345435341")["Contents"]
 
 for object in objects:
     print(object["Key"])
+    s3_resource.delete_object(Bucket="totaltechnolog345435341", Key=object["Key"])
+    
 
