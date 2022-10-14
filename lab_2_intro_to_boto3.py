f4dd6c17c93cecd9236c6f10244857f03be51d3e
Nick <nick@example.com>
2022-10-14 10:24:27 -0700

    create lab_2_intro_to_boto3.py

---
+++ lab_2_intro_to_boto3.py
@@ -0,0 +1,15 @@
+import boto3
+
+client = boto3.client('translate')
+
+def translate_text(): 
+    response = client.translate_text(
+        Text='I am hungry and I will eat', 
+        SourceLanguageCode='en', 
+        TargetLanguageCode='fr' 
+    )
+
+    print(response)
+
+translate_text()
+
