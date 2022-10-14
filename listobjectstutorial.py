3285b92f058e89f74b597aff3c19d49269ebe277
Nick <nick@example.com>
2022-10-13 18:29:33 -0700

    create listobjectstutorial

--- listobjectstutorial.py
+++ listobjectstutorial.py
@@ -2,4 +2,6 @@ import boto3
 s3_resource=boto3.client("s3")
 
 objects = s3_resource.list_objects(Bucket="totaltechnolog345435341")["Contents"]
-print(objects)
+
+for object in objects:
+    print(object['Key'])
