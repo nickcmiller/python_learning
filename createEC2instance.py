f36b694e3f23602861d88e06b604da2d08008347
Nick <nick@example.com>
2022-10-16 11:22:33 -0700

    update createEC2instance

--- createEC2instance.py
+++ createEC2instance.py
@@ -1,10 +1,11 @@
 import boto3 
 ec2_resource = boto3.resource("ec2")
 tags=[{'Key':'Environment','Value':'Dev'}]
-ec2_resource.create_instances(
+result = c2_resource.create_instances(
     ImageId="ami-05fa00d4c63e32376",
     InstanceType="t1.micro",
     MaxCount=1,
     MinCount=1,
     TagSpecifications=[{'ResourceType': 'instance','Tags':tags}]
 )
+print(result)
