0fe05006c690e9b1ba987eb12f6083211b21f6cc
Nick <nick@example.com>
2022-10-16 11:10:58 -0700

    update createEC2instance

--- createEC2instance.py
+++ createEC2instance.py
@@ -1,5 +1,5 @@
 import boto3 
-ec2_resource = boto3.create("ec2")
+ec2_resource = boto3.resource("ec2")
 ec2_resource.create_instances(
     ImageId="ami-05fa00d4c63e32376",
     InstanceType="t1.micro",
