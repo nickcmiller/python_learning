46970679e48ddce9487ed4f74cce56fdd05d39bc
Nick <nick@example.com>
2022-10-16 11:51:17 -0700

    update terminateEC2.py

--- terminateEC2.py
+++ terminateEC2.py
@@ -1,5 +1,11 @@
 import boto3 
-ec2_client=boto3.client("ec2")
-list_instances= ec2_client.describe_instances()
 
-print(len(list_instances["Reservations"]))
+ec2_client=boto3.client("ec2")
+list_instances=ec2_client.describe_instances()
+reservations=list_instances["Reservations"]
+print(len(reservations))
+
+for r in reservations:
+    print(r)
+    print("--------------")
+
