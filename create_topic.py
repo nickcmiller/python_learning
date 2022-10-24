8b378170bd75ee83a806d5cc9bd081363c226a1f
Nick <nick@example.com>
2022-10-23 21:38:12 -0700

    update create_topic.py

--- create_topic.py
+++ create_topic.py
@@ -1,10 +1,10 @@
 import boto3 
 
-name="Test topic"
+name="test-topic"
 endpoint="name@example.com"
 
 sns_client = boto3.client('sns', verify=False)
-topic = sns_client.create_topic(Name="Test")
+topic = sns_client.create_topic(Name=name)
 #print("Created topic ARN ", topic['TopicArn'])
 
 #subscription = sns_client.subscribe(TopicArn=topic['TopicArn'], Protocol="email", Endpoint=endpoint, ReturnSubscriptionArn=True)
