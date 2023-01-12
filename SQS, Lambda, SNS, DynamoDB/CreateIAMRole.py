31efe5ee9281f4f016e6fe301d2729b40ef86ca1
EC2 Default User <miller.nick.c@gmail.com>
2023-01-11 18:46:53 -0800

    added CreateIAMRole.py

---
+++ SQS, Lambda, SNS, DynamoDB/CreateIAMRole.py
@@ -0,0 +1,68 @@
+import boto3
+import json
+
+role_name = "Test-Role"
+policy_name = "test-policy-role"
+description = "A test role"
+
+# Create an IAM client
+iam = boto3.client('iam')
+
+# Create the policy document
+policy_document = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        }
+    ]
+}
+
+# Create the policy
+policy_response = iam.create_policy(
+    PolicyName = policy_name,
+    PolicyDocument = json.dumps(policy_document)
+)
+policy_arn = policy_response['Policy']['Arn']
+
+assume_role_document = { 
+    "Version": "2012-10-17", 
+    "Statement": [ 
+        {   
+            "Effect": "Allow", 
+            "Principal": { 
+                "Service": "lambda.amazonaws.com" 
+            }, 
+            "Action": "sts:AssumeRole"
+        } 
+    ] 
+    
+}
+
+# Create the role
+try:
+    create_response = iam.create_role(
+        RoleName = role_name,
+        AssumeRolePolicyDocument = json.dumps(assume_role_document),
+        Description = description
+    )
+    role_arn = create_response['Role']['Arn']
+    print("Role ARN:", role_arn)
+    
+except Exception as e:
+    print("An role creation error occurred: ", e)
+try:    
+    # Attach the policy to the role
+    attach_response = iam.attach_role_policy(
+        RoleName = role_name,
+        PolicyArn = policy_arn
+    )
+    print("Policy attached successfully to the role")
+except Exception as e:
+    print("An error occurred while attaching the policy to the role: ", e)
--- SQS, Lambda, SNS, DynamoDB/CreateSQSQueue.py
+++ SQS, Lambda, SNS, DynamoDB/CreateSQSQueue.py
@@ -2,15 +2,16 @@ import boto3
 
 # Create an SQS client
 sqs = boto3.client('sqs')
-
-# Create a new SQS queue
-response = sqs.create_queue(
-    QueueName='my-queue',
-    Attributes={
-        'DelaySeconds': '60',
-        'MessageRetentionPeriod': '86400'
-    }
-)
-
-# Print the URL of the new queue
-print(response['QueueUrl'])
+try:
+    # Create a new SQS queue
+    response = sqs.create_queue(
+        QueueName='my-queue',
+        Attributes={
+            'DelaySeconds': '60',
+            'MessageRetentionPeriod': '86400'
+        }
+    )
+    # Print the URL of the new queue
+    print(response['QueueUrl'])
+except Exception as e:
+    print("An error occurred: ", e)
