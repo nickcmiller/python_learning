48a7ee7f52d7d303c50c60ba8f6dd0b89c472b0b
Nick Miller <miller.nick.c@gmail.com>
2023-03-19 21:55:03 -0700

    Add write_record()

--- airtable_chat_gpt_test/airtable_query.py
+++ airtable_chat_gpt_test/airtable_query.py
@@ -9,32 +9,62 @@ load_dotenv()
 # Airtable API key and base ID
 api_key = os.getenv('YOUR_API_KEY')
 base_id = os.getenv('YOUR_BASE_ID')
-
-# Table name and endpoint URL
 table_name = os.getenv('YOUR_TABLE_NAME')
-endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}'
 
-# Set headers and params for API request
-headers = {
-    'Authorization': f'Bearer {api_key}',
-    'Content-Type': 'application/json'
-}
+def get_records(view=None):
+    #Set endpoint for request
+    endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}'
+    
+    # Set headers and params for API request
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json'
+    }
+    
+    # Set params if they exist
+    params = {'view': view} if view else {}
+    
+    # Send API request and get response
+    response = requests.get(endpoint, headers=headers,params=params)
+    
+    # Get the JSON data from the response
+    data = response.json()
+    
+    records = data['records']
+    return records
 
-params = {
-    # 'view': 'YOUR_VIEW_NAME' # Optional: specify a view to filter the records
-}
+#pass a JSON object into new_fields where the keys are the fields and the values are the new content
+def write_record(record_id, new_fields):
+    # endpoint URL
+    endpoint = f'https://api.airtable.com/v0/{base_id}/{table_name}/{record_id}'
+    
+    # Set headers for API request
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json'
+    }
+    
+    # Set new field values for the record
+    # new_fields = {
+    #     'ChatGPT Answer': 'New Value 1'
+    # }
+    
+    # Set the request body to the new fields
+    data = {
+        'fields': new_fields
+    }
+    
+    # Send API request and get response
+    response = requests.patch(endpoint, headers=headers, json=data)
+    
+    # Get the JSON data from the response
+    data = response.json()
+    
+    return data
 
-# Send API request and get response
-response = requests.get(endpoint, headers=headers,params=params)
-
-# Get the JSON data from the response
-data = response.json()
-
-# Print the records
-for record in data['records']:
-    print ("\n---")
-    print(record['fields']['Question'])
-    print(record['id'])
-    print ("---\n")
-    print(record)
+    
+print(get_records()[1])
+# print(write_record('rec2WlBdPAq4M5FxL', {
+#     'ChatGPT Answer': 'New Value 2'
+# }))
     
